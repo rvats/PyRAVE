820611d30df373cf5df317836789c10b4709ee66
Rahul Vats <vats.rahul@gmail.com>
2023-04-11 23:55:53 -0500

    Add files via upload

--- BlurDetection.py
+++ BlurDetection.py
@@ -2,11 +2,12 @@
 from imutils import paths
 import argparse
 import cv2
+import shutil, os
 
 def variance_of_laplacian(image):
-	# compute the Laplacian of the image and then return the focus
-	# measure, which is simply the variance of the Laplacian
-	return cv2.Laplacian(image, cv2.CV_64F).var()
+    # compute the Laplacian of the image and then return the focus
+    # measure, which is simply the variance of the Laplacian
+    return cv2.Laplacian(image, cv2.CV_64F).var()
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -18,19 +19,28 @@ args = vars(ap.parse_args())
 # loop over the input images
 
 for imagePath in paths.list_images(args["images"]):
-	# load the image, convert it to grayscale, and compute the
-	# focus measure of the image using the Variance of Laplacian
-	# method
-	image = cv2.imread(imagePath)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	fm = variance_of_laplacian(gray)
-	text = "Not Blurry"
-	# if the focus measure is less than the supplied threshold,
-	# then the image should be considered "blurry"
-	if fm < args["threshold"]:
-		text = "Blurry"
-	# show the image
-	cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-	cv2.imshow("Image", image)
-	key = cv2.waitKey(0)
+    currentImagePath = os.getcwd()+"\\"+imagePath
+    fileName = os.path.basename(currentImagePath)
+    currentImageDirectoryName = os.path.dirname(currentImagePath)
+    
+    # load the image, convert it to grayscale, and compute the
+    # focus measure of the image using the Variance of Laplacian
+    # method
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    fm = variance_of_laplacian(gray)
+    text = "Not Blurry"
+    # if the focus measure is less than the supplied threshold,
+    # then the image should be considered "blurry"
+    if fm < args["threshold"]:
+        text = "Blurry"
+    # show the image
+    cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+    cv2.imshow("Image", image)
+    newImageDirectoryName = currentImageDirectoryName+"\\"+text
+    newImagePath = currentImageDirectoryName+"\\"+text+"\\"+fileName
+    #shutil.move(currentImagePath, newImageDirectoryName)
+    os.replace(currentImagePath,newImagePath)
+    print("File Moved from",currentImagePath,"to",newImagePath)
+    key = cv2.waitKey(0)
